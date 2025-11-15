ae321db936266cda05bd0e3e2ffd3fe1009ccd3a
Lucas Paulo <lp152659@gmail.com>
2025-11-15 12:14:53 -0300

    chore: Telas de login e cadastro e configuração inicial do shadcn para componentes

---
+++ client/src/app/account/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import { TabsDemo } from "@/components/TabsForm/TabsForm"
+
+export default function Login(){
+    return( 
+        <div className="w-full h-screen flex flex-col justify-center items-center p-3">
+            <TabsDemo />
+        </div>
+    )
+}
---
+++ client/src/app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    
+    <div>
+      <h1>Campus Connect</h1>
+    </div>
+    
+  );
+}
