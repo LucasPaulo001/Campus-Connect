031816a41408532304885d6e6ad6dd26c1e4d46b
Lucas <lp152659@gmail.com>
2026-08-15 14:56:04 -0300

    fix: Configuração de cors e rotas de refresh token e logout

--- server/src/app.ts
+++ server/src/app.ts
@@ -14,7 +14,15 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+
+app.use(cors({
+    origin: [
+        "http://localhost:3000",
+        "https://campus-connect-chi-kohl.vercel.app"
+    ],
+    credentials: true
+}));
+
 app.use(cookieParser());
 
 app.use("/api", router);
--- server/src/modules/users/user.routes.ts
+++ server/src/modules/users/user.routes.ts
@@ -1,9 +1,11 @@
 import { authGuard } from "../../middlewares/AuthGuard.js";
 import {
   LoginController,
+  LogoutController,
   ProfileController,
   ProfileEditController,
   ProfileUsersController,
+  RefreshTokenController,
   RegisterController,
   SearchUserController,
 } from "./user.controller.js";
@@ -16,5 +18,7 @@ userRouter.get("/auth/profile", authGuard, ProfileController);
 userRouter.patch("/auth/profile-edit", authGuard, ProfileEditController);
 userRouter.get("/search/user", authGuard, SearchUserController);
 userRouter.get("/profile/user/:id", authGuard, ProfileUsersController);
+userRouter.post("/logout", LogoutController);
+userRouter.post("/auth/refresh", RefreshTokenController);
 
 export default userRouter;
